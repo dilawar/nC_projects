50f2840816fd9112d38d10757b0bfa8875b6ff6a
Dilawar Singh <dilawars@ncbs.res.in>
2015-02-11 20:31:52 +0530

    Need to add synapse and we are done.

--- simple_cell/moose/simple_network.py
+++ simple_cell/moose/simple_network.py
@@ -16,15 +16,27 @@ __status__           = "Development"
 import moose
 import moose.utils as mu
 import moose.neuroml as nml
+import numpy as np
+
+def createRandomSynapse(compartments, numsynapse, excitory):
+    choices = np.random.choice([0,1], numsynapse, excitory)
+    for c in choices:
+        if c == 0:
+            pass
+        if c == 1:
+            pass
 
 def loadCellModel(path):
     nmlObj = nml.NeuroML()
     projDict, popDict = nmlObj.readNeuroMLFromFile(path)
     network = moose.Neutral('/network')
     network = moose.Neutral('/network')
+    netList = []
     for i in range(100):
         cellPath = moose.Neutral('{}/copy{}'.format(network.path, i))
-        moose.copy(moose.Neutral('/library/SampleCell/'), cellPath, 'SimpleCell')
+        a = moose.copy(moose.Neutral('/library/SampleCell/'), cellPath, 'SimpleCell')
+    comps = moose.wildcardFind('{}/##[TYPE=Compartment]')
+    createRandomSynapse(comps, connectivity=5, excitory=0.3)
     mu.writeGraphviz('network.dot')
     mu.verify()
 
