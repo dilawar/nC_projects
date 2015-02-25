3a3cb9bdb706a130767da4f6ad4a9bbf5585140d
Dilawar Singh <dilawars@ncbs.res.in>
2015-02-25 13:42:56 +0530

    Trying with neuroml version 2.

---
+++ simple_network/run_nml_version2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -x
+#rm -rf *.png
+python ./simple_network.py -f \
+    ./generatedNeuroML/L3Net_17-Feb-15_16-16-18.nml1 \
+    -nc 20 -ns 100\
+    -es 1.0 -sn 0.2 -bm 0.0 \
+    -sw 0.1 -0.0050 -st -0.055 -0.055 \
+    -rt 0.1
+
+#if [ -f network.dot ]; then
+#    echo "graphviz"
+#    neato -Tpng network.dot > network.png
+#fi
