1cb2f957fb5a4be38c8648963ed9c3dc641a5dfd
Dilawar Singh <dilawars@ncbs.res.in>
2015-02-25 15:14:08 +0530

    Added doc to script.

--- simple_network/run.sh
+++ simple_network/run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -x
 #rm -rf *.png
+#nc: Number of cells
+#ns: number of synapse
+#es: Excitatory synapse (fraction of total)
+#sn: Stimulated neurons (fraction of total)
+#bm: Stimulated neurons firing in burst mode (fraction of sn)
+#sw: synaptic weights [Excitatory, Inhib]
+#st: synaptic threshold [Exc, Inhib]
+#rt: run time in seconds.
 python ./simple_network.py -f \
     ./generatedNeuroML/L3Net_17-Feb-15_16-16-18.nml1 \
     -nc 20 -ns 100\
@@ -8,7 +16,3 @@ python ./simple_network.py -f \
     -sw 0.1 -0.0050 -st -0.055 -0.055 \
     -rt 0.1
 
-#if [ -f network.dot ]; then
-#    echo "graphviz"
-#    neato -Tpng network.dot > network.png
-#fi
