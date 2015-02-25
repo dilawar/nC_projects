0af1054183ce245d23b270d0d34dbd85ad8bdce1
Dilawar Singh <dilawars@ncbs.res.in>
2015-02-25 16:55:39 +0530

    100 neurons, 500 synapses.

--- simple_network/run.sh
+++ simple_network/run.sh
@@ -11,15 +11,15 @@
 #sw: synaptic weights [Excitatory, Inhib]
 #st: synaptic threshold [Exc, Inhib]
 #sct: spike detection threshold value
-#in: input pulse, triplet [freq, width (mS), height (nA)]
+#in: input pulse, triplet [freq, width (S), height (A)]
 #rt: run time in seconds.
 
 #rm -rf *.png
 set -x
 python ./simple_network.py -f \
     ./generatedNeuroML/L3Net_17-Feb-15_16-16-18.nml1 \
-    -nc 10 -ns 50\
-    -es 1.0 -sn 0.0 -bm 0.0 \
+    -nc 100 -ns 500\
+    -es 1.0 -sn 0.1 -bm 0.2 \
     -sw 0.1 -0.0050 -st -0.055 -0.055 \
     -in 4.0 0.005 0.000000001 -sct -0.00 -rt 0.5
 
