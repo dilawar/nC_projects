e0af7c856e8369e4ecf00ae66baf9ad4ed36b56b
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-11 23:09:53 +0530

    Added neuroml model. The output off the chart.

--- Ex3_Morphology_copy/load_neuroml_in_moose.py
+++ Ex3_Morphology_copy/load_neuroml_in_moose.py
@@ -3,15 +3,32 @@
 import sys
 import moose
 import moose.neuroml as nml
+import moose.utils as mu
+
+def getSoma(compts):
+    for c in compts:
+        if "Comp_1_0" in c.path:
+            return c
+
+def setRecorder(soma):
+    table = moose.Table('/table')
+    table.connect('requestOut', soma, 'getVm')
+    return table
 
 def main():
     filename = sys.argv[1]
     print("Loading into MOOSE: %s" % filename)
     nml.loadNeuroML_L123(filename)
+    compts = moose.wildcardFind('/cells/##[TYPE=Compartment]')
+    soma = getSoma(compts)
+    table = setRecorder(soma)
     tables = moose.wildcardFind('/##[TYPE=Table]')
+    print soma
     print tables
+    mu.verify()
     moose.reinit()
-    moose.start(1)
+    moose.start(0.2)
+    mu.plotRecords( { 'soma' : table })
 
 if __name__ == '__main__':
     main()
