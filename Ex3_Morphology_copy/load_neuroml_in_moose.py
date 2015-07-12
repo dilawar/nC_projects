7b2e91a1a878ecb3ee2689074003a2859918f193
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-12 18:20:18 +0530

    Not working. Simple inject with all channels does not work in MOOSE.

--- Ex3_Morphology_copy/load_neuroml_in_moose.py
+++ Ex3_Morphology_copy/load_neuroml_in_moose.py
@@ -14,33 +14,37 @@ def getSoma(compts):
     compName = somaPath_ #"Comp_1_0"
     for c in compts:
         if compName in c.path:
-            soma_ = c
             return c
 
-def setRecorder():
+def setRecorder(soma):
     print("Setting records")
     table = moose.Table('/tableA')
-    table.connect('requestOut', soma_, 'getVm')
-    records_[soma_.path] = table
+    table.connect('requestOut', soma, 'getVm')
+    records_[soma.path] = table
 
 def stimulus(compts):
     global soma_
     soma_ = getSoma(compts)
     assert soma_
-    soma_.inject = 1e-10
+    #pulsegen = moose.PulseGen('/pulsegen')
+    #pulsegen.level[0] = 1e-10
+    #pulsegen.connect('output', soma_, 'injectMsg')
+    soma_.inject = 1e-9
+    return soma_
 
 def main():
     filename = sys.argv[1] #'./generatedNeuroML/Generated.net.xml'
     print("Loading into MOOSE: %s" % filename)
     nml.loadNeuroML_L123(filename)
-    for path in moose.wildcardFind('/library/##'):
-        path.tick = -1
-    compts = moose.wildcardFind('/##[TYPE=Compartment]')
-    stimulus(compts)
-    setRecorder()
+    moose.delete('/library')
+
+    compts = moose.wildcardFind('/cells/##[TYPE=Compartment]')
+    soma = stimulus(compts)
+    setRecorder(soma)
+    moose.reinit()
     moose.start(0.1)
     print("Plotting")
-    mu.plotRecords(records_, outfile='soma.png')
+    mu.plotRecords(records_) # outfile='soma.png')
 
 if __name__ == '__main__':
     main()
