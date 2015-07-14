d965465c7e468e233e09e500316fcf1fd6a53377
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-14 17:15:11 +0530

    Injecting into compartment is not enuf.

--- Ex3_Morphology_copy/load_neuroml_in_moose.py
+++ Ex3_Morphology_copy/load_neuroml_in_moose.py
@@ -4,7 +4,6 @@ import sys
 import moose
 import moose.neuroml as nml
 import moose.utils as mu
-import moose.backend as backend
 
 records_ = {}
 somaPath_ = "Comp_1_0"
@@ -22,14 +21,28 @@ def setRecorder(soma):
     table.connect('requestOut', soma, 'getVm')
     records_[soma.path] = table
 
+def verifyTables():
+    print("Verifying result tables")
+    for k in records_:
+        print("%s: %s" % (k, mu.spike_train_simple_stat( records_[k].vector )))
+
 def stimulus(compts):
     global soma_
     soma_ = getSoma(compts)
     assert soma_
-    #pulsegen = moose.PulseGen('/pulsegen')
-    #pulsegen.level[0] = 1e-10
-    #pulsegen.connect('output', soma_, 'injectMsg')
-    soma_.inject = 1e-9
+
+    pulsegen = moose.PulseGen('/pulsegen')
+    pulsegen.level[0] = 1e-9
+    pulsegen.delay[0] = 20e-3
+    pulsegen.width[0] = 40e-3
+
+    pulsegenTable = moose.Table('/pulsegenTab')
+    pulsegenTable.connect('requestOut', pulsegen, 'getOutputValue')
+
+    pulsegen.connect('output', soma_, 'injectMsg')
+
+    records_['pulse_gen'] = pulsegenTable
+    #soma_.inject = 1e-9
     return soma_
 
 def main():
@@ -41,11 +54,13 @@ def main():
     compts = moose.wildcardFind('/cells/##[TYPE=Compartment]')
     soma = stimulus(compts)
     setRecorder(soma)
+
     moose.reinit()
     mu.summary()
     moose.start(0.1)
     print("Plotting")
-    mu.plotRecords(records_) # outfile='soma.png')
+    mu.plotRecords(records_, subplot=True)
+    verifyTables()
 
 if __name__ == '__main__':
     main()
