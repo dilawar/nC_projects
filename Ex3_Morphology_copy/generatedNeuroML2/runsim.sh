93106fe7f52f1abbcae9e6175d6b66dce9b2fd3c
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-15 16:33:02 +0530

    Still no spikes.

---
+++ Ex3_Morphology_copy/generatedNeuroML2/runsim.sh
@@ -0,0 +1,2 @@
+export JNML_HOME=/home1/dilawars/Work/APPLICATIONS/neuroConstruct/jNeuroMLJar
+/home1/dilawars/Work/APPLICATIONS/neuroConstruct/jNeuroMLJar/jnml /home1/dilawars/nC_projects/Ex3_Morphology_copy/generatedNeuroML2/LEMS_Ex3_Morphology_copy.xml
--- Ex3_Morphology_copy/run_moose.sh
+++ Ex3_Morphology_copy/run_moose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 rm -f ./soma.png
-python ./load_neuroml_in_moose.py ./generatedNeuroML/Generated.net.xml | tee _moose.log
+python ./load_neuroml_in_moose.py ./generatedNeuroML/L3Net_15-Jul-15_16-22-40.nml1
 #if [ -f soma.svg ]; then
     #display ./soma.svg
 #fi
