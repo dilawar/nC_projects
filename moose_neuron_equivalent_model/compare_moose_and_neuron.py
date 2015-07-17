56f22774ed32a3e9928803d2ebe364e962c15f95
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-17 17:19:22 +0530

    This works but I need to setup the parameters nicely.

--- moose_neuron_equivalent_model/compare_moose_and_neuron.py
+++ moose_neuron_equivalent_model/compare_moose_and_neuron.py
@@ -19,6 +19,7 @@ import pylab
 import moose_channels as mc
 import moose.utils as mu
 from collections import defaultdict
+import moose.neuroml as nml
 import numpy as np
 
 topology = nx.DiGraph()
@@ -77,23 +78,35 @@ def plot():
     pylab.legend(loc='best', framealpha=0.4)
     pylab.show()
 
-def insertChannels(compt, nrnSeg):
+def insertChannels(compt, segName):
+    nrnSeg = topology.node[segName]['seg']
     compt.length = nrnSeg.x * 1e-6
     compt.Em = nrnSeg.v * 1e-3
     compt.diameter = nrnSeg.diam * 1e-6
-    na = mc.create_na_chan('%sn' % compt.path)
     sarea = np.pi * compt.diameter * compt.diameter
     compt.Rm = 1/(0.3e-3 * 1e4 * sarea)
     compt.Cm = 1e-6 * 1e4 * sarea
+    na = mc.create_na_chan('%sn' % compt.path)
     na.Gbar = 120e-3 * sarea * 1e4
     compt.connect('channel', na, 'channel')
     k = mc.create_k_chan('%sk' % compt.path)
     k.Gbar = 36e-3 * sarea * 1e4
     compt.connect('channel', k, 'channel')
+    #na = moose.element(moose.copy('/library/NaConductance', compt, 'na'))
+    #na.connect('channel', compt, 'channel')
+    #k = moose.element(moose.copy('/library/KConductance', compt, 'k'))
+    #k.connect('channel', compt, 'channel')
 
-def createMOOSE():
+def createMOOSE(nmlfile):
+
+    #nml.loadNeuroML_L123(nmlfile)
+    #for c in moose.wildcardFind('/library/l22/##'):
+    #    c.tick = -1
+    #for c in moose.wildcardFind('/cells/##'):
+    #    c.tick = -1
+
     for i, v in enumerate(topology.nodes()):
-        segname = topology.node[v]['label']
+        segname = v 
         comptPath = mooseName(segname)
         c = moose.Compartment(comptPath)
         moosecompts_[v] = comptPath
@@ -125,10 +138,18 @@ def mooseName(n):
     mooseName = mooseName.translate(None, '][')
     return mooseName
 
+def getNode(seg):
+    for n in topology.nodes():
+        if topology.node[n]['seg'] == seg:
+            return n
+    return None
+
 def main():
     filename = sys.argv[1]
     cell = loadNML(filename)
     soma = None
+    segnames_ = {}
+    segs_ = {}
 
     # For each segment of cell, create a moose Compartment.
     totalSec = 0
@@ -137,37 +158,52 @@ def main():
         secName = sec.hname()
         sec.insert('NaConductance')
         sec.insert('KConductance')
+        length, RA = sec.L, sec.Ra
 
         cable = []
         assert sec.nseg > 0
+
+        # dirty dirty hack.
         for i, seg in enumerate(sec.allseg()):
-            assert seg
+            segnames_[seg] = "%s0" % (secName)
+
+        segsIter = sec.allseg()
+        for i in range(sec.nseg):
+            seg = segsIter.next()
             segName = '%s%s' % (secName, i)
+            segs_[segName] = seg
             print("Segment name: %s" % segName)
-            cable.append(seg)
-            topology.add_node(seg, label=segName, sec_name=secName) 
+            cable.append(segName)
+            topology.add_node(segName
+                    , seg = seg
+                    , length = length / (sec.nseg)
+                    , secRA = RA
+                    , label=segName
+                    , sec_name=secName
+                    , sec = sec
+                    ) 
             nrnsecs_[sec].add(seg)
             nrnsegs_[seg] = sec
             if i > 0:
-                topology.add_edge(cable[i-1], seg)
+                topology.add_edge(cable[i-1], segName)
             else:
-                cable_start_[secName] = seg
+                cable_start_[secName] = segName
+        
+        if not sec.parentseg():
+            soma = sec
 
     # Get section from cell and draw edges.
     print("Total nodes in graph: %s" % topology.number_of_nodes())
     print("Total section in Neuron: %s" % totalSec)
 
-    for sec in cell.allsec():
-        parent = sec.parentseg()
-        if parent:
-            for i, seg in enumerate(sec.allseg()):
-                # Just add one, rest the already connected as cable.
-                topology.add_edge(parent, seg)
-                break
-        else:
-            soma = sec
+    for n in topology.nodes():
+        sec = topology.node[n]['sec']
+        for child in sec.children():
+            childName = cable_start_[child.hname()]
+            topology.add_edge(n, childName)
 
-    createMOOSE()
+    #nx.draw_graphviz(topology , prog='neato', with_labels=False)
+    createMOOSE(filename)
     if soma:
         addStim(soma)
         records_['soma'] = h.Vector()
@@ -178,8 +214,6 @@ def main():
 
     simulate(0.1)
     plot()
-    #nx.draw_graphviz(topology , prog='neato', with_labels=False)
-    pylab.show()
     
 if __name__ == '__main__':
     main()
